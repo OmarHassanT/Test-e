336cd8db3fe79187807515e63bc3888cc3525249
Omar Taradeh <omar.taradeh@foothillsolutions.com>
2025-08-28 21:46:54 +0300

    add ApiErrorResponse to unify the response

---
+++ Test-e.Server/Models/ApiErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_e.Server.Models
+{
+    public class ApiErrorResponse
+    {
+        public string Title { get; set; }     // e.g., "Bad Request"
+        public int Status { get; set; }       // HTTP status code
+        public string Detail { get; set; }    // Description of the error
+        public string Instance { get; set; }  // Request path
+
+    }
+}
